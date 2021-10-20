fe87f0c66490fc0203a3c2ca2aa4204000ea26da
patrick <patrickfosso@yahoo.fr>
2021-10-20 14:44:37 +0100

    add block comments

--- src/main/java/enset/ma/Test.java
+++ src/main/java/enset/ma/Test.java
@@ -1,7 +1,9 @@
 package enset.ma;
-
+/*
+with comments block
+*/
 public class Test{
     public static void main(String[] args){
-        System.out.println("Hello world");
+        System.out.println("Hello world new version");
     }
 }
