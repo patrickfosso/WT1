b1e5dc2e874cbaa7b4c98456ea02c54641862396
patrick <patrickfosso@yahoo.fr>
2021-10-20 20:15:53 +0100

    another block comments

--- src/main/java/enset/ma/Test.java
+++ src/main/java/enset/ma/Test.java
@@ -2,6 +2,9 @@ package enset.ma;
 /*
 with comments block
 */
+/*
+with another block comment
+*/
 public class Test{
     public static void main(String[] args){
         System.out.println("Hello world new version");
