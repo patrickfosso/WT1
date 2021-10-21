93bbd9a98a9be4acb530f0b852eaa30a6491bbfc
fossopatrick <fossopatrick@gmail.com>
2021-10-21 12:24:48 +0100

    print pull request

--- src/main/java/enset/ma/Test2.java
+++ src/main/java/enset/ma/Test2.java
@@ -1,7 +1,14 @@
 package enset.ma;
-
+/*
+*/
 public class Test2{
     public static void main(String[] args){
         System.out.println("There is another file");
+
+        /**
+        value do to pull request
+         
+        */
+        System.out.println("Ask for pull request");
     }
 }
