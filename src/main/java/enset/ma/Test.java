2777a375e5f97ad37dff705cc76b2a0f1540c694
patrick <patrickfosso@yahoo.fr>
2021-10-21 09:53:34 +0100

    add new console to print

--- src/main/java/enset/ma/Test.java
+++ src/main/java/enset/ma/Test.java
@@ -8,5 +8,6 @@ with another block comment
 public class Test{
     public static void main(String[] args){
         System.out.println("Hello world new version");
+        System.out.println("Another ");
     }
 }
